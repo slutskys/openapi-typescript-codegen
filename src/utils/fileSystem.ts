dccbe5ed75333c73c173ff81b89d3c40b3300335
Quentin Fox <quentinbennettfox@gmail.com>
2021-08-20 20:11:26 -0400

    run prettier on files before writing

--- src/utils/fileSystem.ts
+++ src/utils/fileSystem.ts
@@ -2,12 +2,30 @@ import { copyFile as __copyFile, exists as __exists, readFile as __readFile, wri
 import mkdirp from 'mkdirp';
 import rimraf from 'rimraf';
 import { promisify } from 'util';
+import prettier from 'prettier';
 
 // Wrapped file system calls
+
 export const readFile = promisify(__readFile);
-export const writeFile = promisify(__writeFile);
+const writeFileRaw = promisify(__writeFile);
 export const copyFile = promisify(__copyFile);
 export const exists = promisify(__exists);
+
+type WriteFile = typeof writeFileRaw;
+
+export const writeFile: WriteFile = async (path, data, options) => {
+    let formattedData = data;
+
+    if (typeof data === 'string' && typeof path === 'string') {
+        const prettierConfig = await prettier.resolveConfig(path);
+        formattedData = prettier.format(data, {
+            ...prettierConfig,
+            parser: 'typescript',
+        });
+    }
+
+    return await writeFileRaw(path, formattedData, options);
+};
 
 // Re-export from mkdirp to make mocking easier
 export const mkdir = mkdirp;
